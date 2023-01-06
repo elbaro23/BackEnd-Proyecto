58d028527dd05092186a6ccb07cee4e9679b03e7
Oscar Baroni <elbaro_oab@hotmail.com>
2023-01-06 18:40:03 -0300

    modificaciones de las URL de los Controller

--- src/main/java/com/portfoliooab/OAB/Controller/CEducacion.java
+++ src/main/java/com/portfoliooab/OAB/Controller/CEducacion.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = {"https://frontend-oab.web.app", "http://localhost:4200"})
+@CrossOrigin(origins = {"https://frontend-proyecto-oab.web.app", "http://localhost:4200"})
 
 public class CEducacion {
 
--- src/main/java/com/portfoliooab/OAB/Security/Controller/AuthController.java
+++ src/main/java/com/portfoliooab/OAB/Security/Controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"https://frontend-oab.web.app", "http://localhost:4200"})
+@CrossOrigin(origins = {"https://frontend-proyecto-oab.web.app", "http://localhost:4200"})
 
 public class AuthController {
 
